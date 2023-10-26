269f7685dcef53d92620d26c92cc4bb51f9e082b
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2023-10-26 23:27:10 +0200

    cambiamenti miei

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -2,72 +2,73 @@ import { Component } from "react";
 
 import { Button, Form } from "react-bootstrap";
 
+class AddComment extends Component {
+  state = {
+    lista: {
+      comment: "",
+      rate: "",
+      elementId: "",
+    },
+  };
 
-
-class AddComment extends Component{
-
-    state={
-     lista:{
-         comment: '',
-         rate: '',
-         elementId:''
-     }
-        
-    }
-
-   AggiuntaCommenti=(comment,value)=>{
+  AggiuntaCommenti = (comment, value) => {
     this.setState({
-         ...this.state.lista,
-         [comment]:value,
-   })
+      ...this.state.lista,
+      [comment]: value,
+    });
 
-    BottonSubmit = (e) => {
-        e.preventDefault()
-       
-        
-        fetch('https://striveschool-api.herokuapp.com/api/comments/'+ this.state.lista.elementId , {
-        
-          method: 'POST',
-          body: JSON.stringify(this.state.lista),
-          headers: {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTNhNGQyNGY2ZTNkZDAwMTQ5NWU0MzUiLCJpYXQiOjE2OTgzMTk2NTIsImV4cCI6MTY5OTUyOTI1Mn0.jnxrboAz7lzLuBfwHtCzGBSV82saRHrcMHzB2aaIpNI",
-            'Content-Type': 'application/json',
-          },
-        })
-          .then((res) => {
-            console.log('risposta', res)
-            if (res.ok) {
-       
-            this.setState({
-             lista: {
-               comment: '',
-               rate: '',
-               elementId:''
-          },
-          
-          
-        })
-       }   else {
-              throw new Error("C'è stato un errore nella get")
-            }
+    // BottonSubmit = (e) => {
+    //   e.preventDefault();
 
-          })
-          .catch((err) => {
-            console.log('errore', err)
-          })
-  } 
+    //   fetch(
+    //     "https://striveschool-api.herokuapp.com/api/comments/" +
+    //       this.state.lista.elementId,
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify(this.state.lista),
+    //       headers: {
+    //         Authorization:
+    //           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTNhNGQyNGY2ZTNkZDAwMTQ5NWU0MzUiLCJpYXQiOjE2OTgzMTk2NTIsImV4cCI6MTY5OTUyOTI1Mn0.jnxrboAz7lzLuBfwHtCzGBSV82saRHrcMHzB2aaIpNI",
+    //         "Content-Type": "application/json",
+    //       },
+    //     }
+    //   )
+    //     .then((res) => {
+    //       console.log("risposta", res);
+    //       if (res.ok) {
+    //         this.setState({
+    //           lista: {
+    //             comment: "",
+    //             rate: "",
+    //             elementId: "",
+    //           },
+    //         });
+    //       } else {
+    //         throw new Error("C'è stato un errore nella get");
+    //       }
+    //     })
+    //     .catch((err) => {
+    //       console.log("errore", err);
+    //     });
+    // };
 
-        <Form onSubmit={this.BottonSubmit} key={this.state.lista.elementId}>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    <Form onSubmit={this.BottonSubmit} key={this.state.lista.elementId}>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label></Form.Label>
-        <Form.Control type="number" placeholder="Rate da 1 a 5" value={this.state.lista.rate} />
-       <Form.Control type="text" placeholder="Comment" value={this.state.lista.comment} />
-       <Button>Submit</Button>
+        <Form.Control
+          type="number"
+          placeholder="Rate da 1 a 5"
+          value={this.state.lista.rate}
+        />
+        <Form.Control
+          type="text"
+          placeholder="Comment"
+          value={this.state.lista.comment}
+        />
+        <Button>Submit</Button>
       </Form.Group>
-      </Form>
-        
-    
-}}
+    </Form>;
+  };
+}
 
-
-export default AddComment
+export default AddComment;
--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,79 +1,64 @@
 import { Component } from "react";
-import { Col, Container, ListGroup,Row } from "react-bootstrap";
+import { Col, Container, ListGroup, Row } from "react-bootstrap";
 
+class CommentArea extends Component {
+  state = {
+    commenti: [],
+  };
 
-class CommentArea extends Component{
-   state={
-     commenti:[],
-   }
-
-   FeedbackArea=()=>{
-     fetch("https://striveschool-api.herokuapp.com/api/comments/0316438960" ,{
+  FeedbackArea = () => {
+    try {
+      fetch("https://striveschool-api.herokuapp.com/api/comments/0316438960", {
         headers: {
-"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTNhNGQyNGY2ZTNkZDAwMTQ5NWU0MzUiLCJpYXQiOjE2OTgzMTk2NTIsImV4cCI6MTY5OTUyOTI1Mn0.jnxrboAz7lzLuBfwHtCzGBSV82saRHrcMHzB2aaIpNI"
-}
-  
-     })
-     .then(response=>{
-        if (response.ok) {
-            return response.json()
-        }else{
-            throw new Error('Errore nella Get')
-        }
-     })
-    
-     .then(com=>{
-      
-       this.setState({
-         commenti:com
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTNhNGQyNGY2ZTNkZDAwMTQ5NWU0MzUiLCJpYXQiOjE2OTgzMTk2NTIsImV4cCI6MTY5OTUyOTI1Mn0.jnxrboAz7lzLuBfwHtCzGBSV82saRHrcMHzB2aaIpNI",
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Errore nella Get");
+          }
+        })
 
-       })
-       console.log("comment",com)
-     })
+        .then((com) => {
+          this.setState({
+            commenti: com,
+          });
+          console.log("comment", com);
+        })
 
-     .catch(err=>{
-        console.log("errore specificato",err)
-     })
+        .catch((err) => {
+          console.log("errore", err);
+        });
+    } catch (error) {
+      console.log("errore", error);
+    }
+  };
 
-     
-   }
+  componentDidMount() {
+    this.FeedbackArea();
+  }
 
-   componentDidMount(){
-    this.FeedbackArea()
-   }
-
-   render(){
-    
-   
-    return(
-
-    <Container>
-      <Row>
-        <Col  xs={12} md={4}> 
-        
-            {this.state.commenti.map(com=>{
-                return(
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col xs={12} md={4}>
+            {this.state.commenti.map((com) => {
+              return (
                 <ListGroup key={com.elementId}>
                   <ListGroup.Item>{com.comment}</ListGroup.Item>
                   <ListGroup.Item>{com.rate}</ListGroup.Item>
-                </ListGroup> 
-            
-                )
-            })
-
-            }
-            
-        
-        </Col>
-      </Row>
-    </Container>
-   
-    
-    )
-    
-   }
-   
+                </ListGroup>
+              );
+            })}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
-
-export default CommentArea
+export default CommentArea;
